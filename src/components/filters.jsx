e8d85577a9006c761fd4b270794a426965e96fa6
laysonaljon <aljonlayson21@gmail.com>
2025-01-15 09:58:54 +0800

    Update Filter and table styling

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import Autocomplete from './autocomplete'; // Ensure this path is correct
+import Autocomplete from './autocomplete';
 
-const Filters = ({ onFilter, onClear, filters, selectedFilters }) => {
+const Filters = ({ 
+  onFilter, 
+  onClear, 
+  filters, 
+  selectedFilters, 
+  count, 
+  total 
+}) => {
   const [localSelectedFilters, setLocalSelectedFilters] = useState({});
 
   useEffect(() => {
@@ -18,39 +25,44 @@ const Filters = ({ onFilter, onClear, filters, selectedFilters }) => {
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full items-center"> {/* Responsive layout */}
-      {filters.map((filter, index) =>
-        filter.type === 'dropdown' && index < 2 && (
-          <div key={filter.id} className="col-span-1">
-            <Autocomplete
-              label={filter.label}
-              options={filter.options}
-              placeholder={filter.placeholder || 'Select suburb...'}
-              selectedValues={localSelectedFilters[filter.id] || []}
-              onChange={(values) => handleFilterChange(filter.id, values)}
-              className="w-full"
-            />
-          </div>
-        )
-      )}
-      <div className="col-span-1 flex space-x-2">
-        <button
-          onClick={applyFilters}
-          className="flex-1 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-600"
-          style={{ height: "50px", marginTop: "5px" }} // Adjust marginTop for vertical alignment
-        >
-          Apply Filters
-        </button>
-        <button
-          onClick={() => {
-            onClear();
-            setLocalSelectedFilters({});
-          }}
-          className="flex-1 p-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-500"
-          style={{ height: "50px", marginTop: "5px" }} // Adjust marginTop for vertical alignment
-        >
-          Clear Filters
-        </button>
+    <div className="bg-gray-100 dark:bg-gray-700 rounded-lg p-4 w-full"> {/* Gray background for light and dark modes */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center"> {/* Responsive layout */}
+        {filters.map((filter, index) =>
+          filter.type === 'dropdown' && index < 2 && (
+            <div key={filter.id} className="col-span-1">
+              <Autocomplete
+                label={filter.label}
+                options={filter.options}
+                placeholder={filter.placeholder || 'Select suburb...'}
+                selectedValues={localSelectedFilters[filter.id] || []}
+                onChange={(values) => handleFilterChange(filter.id, values)}
+                className="w-full"
+              />
+            </div>
+          )
+        )}
+        <div className="col-span-1 flex space-x-2">
+          <button
+            onClick={applyFilters}
+            className="flex-1 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-600"
+            style={{ height: "50px", marginTop: "5px" }} // Adjust marginTop for vertical alignment
+          >
+            Apply Filters
+          </button>
+          <button
+            onClick={() => {
+              onClear();
+              setLocalSelectedFilters({});
+            }}
+            className="flex-1 p-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 dark:bg-gray-600 dark:text-gray-300 dark:hover:bg-gray-500"
+            style={{ height: "50px", marginTop: "5px" }} // Adjust marginTop for vertical alignment
+          >
+            Clear Filters
+          </button>
+        </div>
+      </div>
+      <div className="text-sm text-gray-500 text-center mb-4 mt-4 dark:text-gray-300">
+        View {count} of {total}
       </div>
     </div>
   );
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -64,99 +64,98 @@ const Table = ({
     const isDescending = sortKey === `-${colKey}`;
   
     if (!isActiveSort) {
-      return <span className="text-gray-400 dark:text-gray-500 ms-1">▲</span>; // Light gray for light mode, slightly darker for dark mode
+      return <span className="text-gray-400 dark:text-gray-500 ms-1">▲</span>;
     }
     return isDescending ? (
-      <span className="text-black dark:text-white ms-1">▼</span> // Black for light mode, white for dark mode
+      <span className="text-white ms-1">▼</span>
     ) : (
-      <span className="text-black dark:text-white ms-1">▲</span> // Black for light mode, white for dark mode
+      <span className="text-white ms-1">▲</span>
     );
   };  
 
   return (
-    <div className="relative overflow-x-auto w-full">
-  <h2 className="text-xl font-bold mb-4">{title}</h2>
+    <div className="relative w-full">
+      <h2 className="text-xl font-bold mb-4">{title}</h2>
 
-  {filters && filters.length > 0 && (
-    <div className="flex justify-between items-center mb-4">
-      <Filters 
-        onFilter={setSelectedFilters}
-        onClear={() => setSelectedFilters({})}
-        filters={filters} 
-        selectedFilters={selectedFilters}
-      />
-    </div>
-  )}
+      {filters && filters.length > 0 && (
+        <div className="flex justify-between items-center mb-4">
+          <Filters 
+            onFilter={setSelectedFilters}
+            onClear={() => setSelectedFilters({})}
+            filters={filters} 
+            selectedFilters={selectedFilters}
+            total={meta.total_items}
+            count={data.length}
+          />
+        </div>
+      )}
 
-  <div className="text-sm text-gray-500 text-center mb-4">
-    View {data.length} of {meta.total_items}
-  </div>
-
-  <table className="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
-    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-      <tr>
-        {header.map((col) => (
-          <th
-            key={col.id}
-            scope="col"
-            className="px-6 py-3"
-            style={{ width: col.minSize }}
-          >
-            <div className="flex items-center">
-              {col.label}
-              {col.sortKey && (
-                <button
-                  className="ml-1 text-gray-400 hover:text-black dark:hover:text-white"
-                  onClick={() => handleSort(col.sortKey)}
-                >
-                  {renderSortIcon(col.sortKey)}
-                </button>
-              )}
-            </div>
-          </th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {data.map((item) => (
-        <tr
-          key={item._id}
-          className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer"
-          onClick={() => onRowClick(item._id)}
-        >
-          {header.map((col) => (
-            <td
-              key={`${item._id}-${col.id}`}
-              className="px-6 py-4"
-              style={{ width: col.minSize }}
+      <table className="table-auto w-full text-sm text-left border-collapse rounded-lg overflow-hidden"> 
+        <thead className="text-xs uppercase bg-gray-500 text-white dark:bg-gray-600 dark:text-white">
+          <tr>
+            {header.map((col) => (
+              <th
+                key={col.id}
+                scope="col"
+                className="px-6 py-3 first:rounded-tl-lg last:rounded-tr-lg" // Add rounded corners to the first and last cells
+                style={{ width: col.minSize }}
+              >
+                <div className="flex items-center">
+                  {col.label}
+                  {col.sortKey && (
+                    <button
+                      className="ml-1 text-gray-400 hover:text-white dark:hover:text-gray-300"
+                      onClick={() => handleSort(col.sortKey)}
+                    >
+                      {renderSortIcon(col.sortKey)}
+                    </button>
+                  )}
+                </div>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-200"> {/* Rounded corners on the bottom */}
+          {data.map((item, index) => (
+            <tr
+              key={item._id}
+              className={`border-b cursor-pointer dark:border-gray-700 ${
+                index === data.length - 1 ? 'last:rounded-bl-lg last:rounded-br-lg' : ''
+              }`}
+              onClick={() => onRowClick(item._id)}
             >
-              {col.type === 'date'
-                ? new Intl.DateTimeFormat('en-US', {
-                    year: 'numeric',
-                    month: '2-digit',
-                    day: '2-digit',
-                  }).format(new Date(item[col.sortKey]))
-                : item[col.sortKey]}
-            </td>
+              {header.map((col) => (
+                <td
+                  key={`${item._id}-${col.id}`}
+                  className="px-6 py-4"
+                  style={{ width: col.minSize }}
+                >
+                  {col.type === 'date'
+                    ? new Intl.DateTimeFormat('en-US', {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                      }).format(new Date(item[col.sortKey]))
+                    : item[col.sortKey]}
+                </td>
+              ))}
+            </tr>
           ))}
-        </tr>
-      ))}
-    </tbody>
-  </table>
+        </tbody>
+      </table>
 
-  {meta.next_page !== null && (
-    <div className="flex justify-center mt-4">
-      <span
-        className="text-blue-500 cursor-pointer hover:underline"
-        onClick={() => setCurrentPage((prev) => prev + 1)}
-      >
-        Load More
-      </span>
+
+      {meta.next_page !== null && (
+        <div className="flex justify-center mt-4">
+          <span
+            className="text-blue-500 cursor-pointer hover:underline"
+            onClick={() => setCurrentPage((prev) => prev + 1)}
+          >
+            Load More
+          </span>
+        </div>
+      )}
     </div>
-  )}
-
-</div>
-
   );
 };
 
