511702e35a93b58f240776df166634c767319db9
laysonaljon <aljonlayson21@gmail.com>
2025-03-05 12:57:27 +0800

    update footer link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="px-4 pb-4">
       <div className="bg-gray-300 text-sm dark:bg-gray-700 rounded-lg p-4 w-full">
         <nav className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-24 mb-4">
-          <a href="/terms-of-use" className="font-semibold text-gray-600 hover:text-white">
+          <Link to="/terms-of-use" className="font-semibold text-gray-600 hover:text-white">
             Terms of Use
-          </a>
-          <a href="/privacy-policy" className="font-semibold text-gray-600 hover:text-white">
+          </Link>
+          <Link to="/privacy-policy" className="font-semibold text-gray-600 hover:text-white">
             Privacy Policy
-          </a>
-          <a href="/about" className="font-semibold text-gray-600 hover:text-white">
+          </Link>
+          <Link to="/about" className="font-semibold text-gray-600 hover:text-white">
             About
-          </a>
-          <a href="/disclaimer" className="font-semibold text-gray-600 hover:text-white">
+          </Link>
+          <Link to="/disclaimer" className="font-semibold text-gray-600 hover:text-white">
             Disclaimer
-          </a>
+          </Link>
         </nav>
         <div className="text-sm italic text-gray-600 text-center">
           <p>
