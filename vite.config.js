73cc387460711b49984f06f4ce095401d3009479
laysonaljon <aljonlayson21@gmail.com>
2025-03-05 12:42:25 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3002,
+    historyApiFallback: true, // Ensures that Vite serves index.html for any route
     mimeTypes: {
       js: 'application/javascript',
     },
